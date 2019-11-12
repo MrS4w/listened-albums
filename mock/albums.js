07b578d746aef6a129f234e30dba126c6535a21e
Victor <adsvictor@outlook.com>
2019-11-11 23:49:30 -0300

    Add first version

---
+++ mock/albums.js
@@ -0,0 +1,84 @@
+const albums = [
+  {
+    artist: 'Green Day',
+    name: '¡TRÉ!',
+    releaseYear: 2019,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/2/2e/Green_Day_-_Tr%C3%A9%21_cover.jpg/220px-Green_Day_-_Tr%C3%A9%21_cover.jpg'
+  },
+  {
+    artist: 'Three Days Grace',
+    name: 'Human',
+    releaseYear: 2015,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/1/1b/Three_Days_Grace_Human.jpg'
+  },
+  {
+    artist: 'The Doors',
+    name: 'L.A. Woman',
+    releaseYear: 1971,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/4/41/The_Doors_-_L.A._Woman.jpg/220px-The_Doors_-_L.A._Woman.jpg'
+  },
+  {
+    artist: 'Blink-182',
+    name: 'Enema Of The State',
+    releaseYear: 1999,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Blink-182_-_Enema_of_the_State_cover.jpg/220px-Blink-182_-_Enema_of_the_State_cover.jpg'
+  },
+  {
+    artist: 'Charlie Brown Jr.',
+    name: 'Acústico (Ao Vivo)',
+    releaseYear: 2003,
+    cover:
+      'https://http2.mlstatic.com/cd-charlie-brown-jr-acustico-mtv-D_NQ_NP_482401-MLB20316078302_062015-F.jpg'
+  },
+  {
+    artist: 'Barns Courtney',
+    name: 'The Attractions Of Youth',
+    releaseYear: 2017,
+    cover: 'https://e.snmc.io/i/300/w/451877f821fe772b57bcb035dec6bc54/6687174'
+  },
+  {
+    artist: 'Panic! at the Disco',
+    name: 'Pretty. Odd.',
+    releaseYear: 2008,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/0/09/PatD_-_Pretty._Odd..jpg'
+  },
+  {
+    artist: 'Misfits',
+    name: 'Project 1950',
+    releaseYear: 2003,
+    cover:
+      'https://images-na.ssl-images-amazon.com/images/I/51l1RbxuuDL._SY355_.jpg'
+  },
+  {
+    artist: 'Audioslave',
+    name: 'Audioslave',
+    releaseYear: 2002,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/a/ac/Audioslave_-_Audioslave.jpg'
+  },
+  {
+    artist: 'Matanza',
+    name: 'Thunder Dope',
+    releaseYear: 2012,
+    cover: 'https://i.imgur.com/kW5Z9Bd.png'
+  },
+  {
+    artist: 'Foo Fighters',
+    name: 'There Is Nothing Left To Lose',
+    releaseYear: 1999,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/8/8b/Foo_Fighters_-_There_Is_Nothing_Left_to_Lose.jpg'
+  },
+  {
+    artist: 'Linkin Park',
+    name: 'Hybrid Theory',
+    releaseYear: 2000,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Linkin_park_hybrid_theory.jpg/220px-Linkin_park_hybrid_theory.jpg'
+  }
+];
