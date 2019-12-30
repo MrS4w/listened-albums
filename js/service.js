c8c9b2ba26f2802e4a0b653a2745ed29b1462e06
Victor <adsvictor@outlook.com>
2019-12-30 09:56:16 -0300

    Format code

--- js/service.js
+++ js/service.js
@@ -3,14 +3,14 @@ const target = document.querySelector('#albums');
 albums.forEach((album, index) => {
   target.innerHTML += `
     <div class="card col-lg-2 col-md-3 p-2">
-        <img
+      <img
         src="${album.cover}"
         class="card-img-top"
-        />
-        <span class="text-center">#${index + 1}</span>
-        <h5 class="card-title text-center">${album.name}</h5>
-        <h6 class="text-center">Artist: ${album.artist}</h6>
-        <h6 class=" text-center">Released: ${album.releaseYear}</h6>
+      />
+      <span class="text-center">#${index + 1}</span>
+      <h5 class="card-title text-center">${album.name}</h5>
+      <h6 class="text-center">Artist: ${album.artist}</h6>
+      <h6 class=" text-center">Released: ${album.releaseYear}</h6>
     </div>
     `;
 });
