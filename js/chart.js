a7281fff9f44932ad160ffc6edd1c087c8fb2340
Victor <adsvictor@outlook.com>
2020-01-02 12:48:35 -0300

    Add The Beatles - Please Please Me | Add 2020.html

--- js/chart.js
+++ js/chart.js
@@ -5,6 +5,7 @@ function getAlbumsQuantityByYear(firstYear, lastYear) {
   return albumsQuantity;
 }
 
+const sixty = getAlbumsQuantityByYear(1960, 1969);
 const seventy = getAlbumsQuantityByYear(1970, 1979);
 const eighty = getAlbumsQuantityByYear(1980, 1989);
 const ninety = getAlbumsQuantityByYear(1990, 1999);
@@ -16,12 +17,12 @@ const context = document.getElementById('myChart').getContext('2d');
 const chart = new Chart(context, {
   type: 'pie',
   data: {
-    labels: ["70's", "80's", "90's", "2000's", "2010's"],
+    labels: ["60's", "70's", "80's", "90's", "2000's", "2010's"],
     datasets: [
       {
-        backgroundColor: ['#1e2c4f', '#007', '#f92', '#700', '#077'],
+        backgroundColor: ['#af400f', '#1e2c4f', '#007', '#f92', '#700', '#077'],
         borderColor: '#fff',
-        data: [seventy, eighty, ninety, twoThousand, twoThousandTen]
+        data: [sixty, seventy, eighty, ninety, twoThousand, twoThousandTen]
       }
     ]
   },
---
+++ mock/albums2020.js
@@ -0,0 +1,9 @@
+const albums = [
+  {
+    artist: 'The Beatles',
+    name: 'Please Please Me',
+    releaseYear: 1963,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/pt/thumb/4/49/The_Beatles_-_Please_Please_Me.jpg/220px-The_Beatles_-_Please_Please_Me.jpg'
+  }
+];
