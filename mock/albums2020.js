a229ef883fb50e339712a446807a5aeb3afffe38
Victor <adsvictor@outlook.com>
2020-01-02 15:41:15 -0300

    Add Three Days Grace - Three Days Grace

--- mock/albums2020.js
+++ mock/albums2020.js
@@ -12,5 +12,12 @@ const albums = [
     releaseYear: 1963,
     cover:
       'https://upload.wikimedia.org/wikipedia/pt/thumb/b/bc/The_Beatles_-_With_the_Beatles.jpg/220px-The_Beatles_-_With_the_Beatles.jpg'
+  },
+  {
+    artist: 'Three Days Grace',
+    name: 'Three Days Grace',
+    releaseYear: 2003,
+    cover:
+      'https://is1-ssl.mzstatic.com/image/thumb/Music/v4/04/79/6b/04796b5e-e015-4c94-05a0-3cacefc1e307/source/1200x1200bb.jpg'
   }
 ];
