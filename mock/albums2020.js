461fa71eeb081175eff4f586bf638432dc91b1f8
Victor <adsvictor@outlook.com>
2020-01-02 14:53:50 -0300

    Add The Beatles - With The Beatles

--- mock/albums2020.js
+++ mock/albums2020.js
@@ -5,5 +5,12 @@ const albums = [
     releaseYear: 1963,
     cover:
       'https://upload.wikimedia.org/wikipedia/pt/thumb/4/49/The_Beatles_-_Please_Please_Me.jpg/220px-The_Beatles_-_Please_Please_Me.jpg'
+  },
+  {
+    artist: 'The Beatles',
+    name: 'With The Beatles',
+    releaseYear: 1963,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/pt/thumb/b/bc/The_Beatles_-_With_the_Beatles.jpg/220px-The_Beatles_-_With_the_Beatles.jpg'
   }
 ];
