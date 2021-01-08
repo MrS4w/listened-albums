dbc67cecb41258a4f5c6dd9645e4000d196a32d8
Victor <adsvictor@outlook.com>
2021-01-08 03:59:26 -0300

    Add Agents of Fortune - Blue Öyster Cult

--- mock/albums2021.js
+++ mock/albums2021.js
@@ -6,4 +6,11 @@ const albums = [
     cover:
       'https://upload.wikimedia.org/wikipedia/en/thumb/4/44/Whosnext.jpg/220px-Whosnext.jpg',
   },
+  {
+    artist: 'Blue Öyster Cult',
+    name: 'Agents of Fortune',
+    releaseYear: 1976,
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/5/57/BlueOysterCultAgentsofFortune.jpg',
+  },
 ];
