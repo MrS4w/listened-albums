4600518949f9e73c93273b8b2adf163acc3ac184
Victor <adsvictor@outlook.com>
2021-07-08 17:01:59 -0300

    Optimize code

--- js/chart.js
+++ js/chart.js
@@ -15,58 +15,65 @@ const twoThousandTwenty = getAlbumsQuantityByYear(2020, 2029);
 
 const context = document.getElementById('myChart').getContext('2d');
 
-const labels = getLabels();
-const colors = getColors();
-const data = getData();
-
-function getLabels() {
-  let labelsArray = [];
-  if (sixty) labelsArray.push("60's");
-  if (seventy) labelsArray.push("70's");
-  if (eighty) labelsArray.push("80's");
-  if (ninety) labelsArray.push("90's");
-  if (twoThousand) labelsArray.push("2000's");
-  if (twoThousandTen) labelsArray.push("2010's");
-  if (twoThousandTwenty) labelsArray.push("2020's");
-
-  return labelsArray;
-}
-
-function getColors() {
-  let colorsArray = [];
-  if (sixty) colorsArray.push('#af400f');
-  if (seventy) colorsArray.push('#1e2c4f');
-  if (eighty) colorsArray.push('#007');
-  if (ninety) colorsArray.push('#f92');
-  if (twoThousand) colorsArray.push('#700');
-  if (twoThousandTen) colorsArray.push('#077');
-  if (twoThousandTwenty) colorsArray.push('#666');
-
-  return colorsArray;
-}
-
 function getData() {
   let dataArray = [];
-  if (sixty) dataArray.push(sixty);
-  if (seventy) dataArray.push(seventy);
-  if (eighty) dataArray.push(eighty);
-  if (ninety) dataArray.push(ninety);
-  if (twoThousand) dataArray.push(twoThousand);
-  if (twoThousandTen) dataArray.push(twoThousandTen);
-  if (twoThousandTwenty) dataArray.push(twoThousandTwenty);
+  let colorsArray = [];
+  let labelsArray = [];
 
-  return dataArray;
+  if (sixty) {
+    labelsArray.push("60's");
+    colorsArray.push('#af400f');
+    dataArray.push(sixty);
+  }
+
+  if (seventy) {
+    labelsArray.push("70's");
+    colorsArray.push('#1e2c4f');
+    dataArray.push(seventy);
+  }
+
+  if (eighty) {
+    labelsArray.push("80's");
+    colorsArray.push('#007');
+    dataArray.push(eighty);
+  }
+
+  if (ninety) {
+    labelsArray.push("90's");
+    colorsArray.push('#f92');
+    dataArray.push(ninety);
+  }
+
+  if (twoThousand) {
+    labelsArray.push("2000's");
+    colorsArray.push('#700');
+    dataArray.push(twoThousand);
+  }
+
+  if (twoThousandTen) {
+    labelsArray.push("2010's");
+    colorsArray.push('#077');
+    dataArray.push(twoThousandTen);
+  }
+
+  if (twoThousandTwenty) {
+    labelsArray.push("2020's");
+    colorsArray.push('#666');
+    dataArray.push(twoThousandTwenty);
+  }
+
+  return { labels: labelsArray, colors: colorsArray, data: dataArray };
 }
 
 const chart = new Chart(context, {
   type: 'pie',
   data: {
-    labels: labels,
+    labels: getData()['labels'],
     datasets: [
       {
-        backgroundColor: colors,
+        backgroundColor: getData()['colors'],
         borderColor: '#fff',
-        data: data,
+        data: getData()['data'],
       },
     ],
   },
