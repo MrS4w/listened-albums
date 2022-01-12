98298f697752b85b1b990c5bc7b76fd8dc04807b
Victor <adsvictor@outlook.com>
2022-01-12 11:38:09 -0300

    Add Demon Days

--- mock/albums2022.js
+++ mock/albums2022.js
@@ -5,4 +5,10 @@ const albums = [
     releaseYear: 1975,
     cover: 'https://i.scdn.co/image/ab67616d0000b2739199bdfc160aa8d63a57040a',
   },
+  {
+    artist: 'Gorillaz',
+    name: 'Demon Days',
+    releaseYear: 2005,
+    cover: 'https://i.scdn.co/image/ab67616d0000b27319d85a472f328a6ed9b704cf',
+  },
 ];
