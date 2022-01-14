bae34ced160891a60b4d4d6c32d9b172c34d3f46
Victor <adsvictor@outlook.com>
2022-01-13 22:21:09 -0300

    Add War

--- mock/albums2022.js
+++ mock/albums2022.js
@@ -11,4 +11,10 @@ const albums = [
     releaseYear: 2005,
     cover: 'https://i.scdn.co/image/ab67616d0000b27319d85a472f328a6ed9b704cf',
   },
+  {
+    artist: 'U2',
+    name: 'War',
+    releaseYear: 1983,
+    cover: 'https://i.scdn.co/image/ab67616d0000b273ba323a5636c60a606a263a5e',
+  },
 ];
