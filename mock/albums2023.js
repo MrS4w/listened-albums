023bbaa1e676d7c251369f6788f1f82bad960e6f
Victor <adsvictor@outlook.com>
2023-04-27 00:48:35 -0300

    :sparkles: feat: add 2023 data

---
+++ mock/albums2023.js
@@ -0,0 +1,44 @@
+const albums = [
+  {
+    artist: 'Rush',
+    name: 'Exit... Stage Left',
+    releaseYear: 1981,
+    cover: 'https://i.scdn.co/image/ab67616d0000b273c61cd06a6ad807bbc98dbf4a',
+  },
+  {
+    artist: 'Weezer',
+    name: 'Weezer (Teal Album)',
+    releaseYear: 2019,
+    cover: 'https://i.scdn.co/image/ab67616d0000b27323473d8f0c8669583307532e',
+  },
+  {
+    artist: 'Sociedade da Grã-Ordem Kavernista',
+    name: 'Sessão das 10',
+    releaseYear: 1971,
+    cover: 'https://i.scdn.co/image/ab67616d00001e02110342fe6a0ecf13bcae7f45',
+  },
+  {
+    artist: 'Gene Simmons',
+    name: 'Gene Simmons',
+    releaseYear: 1978,
+    cover: 'https://i.scdn.co/image/ab67616d0000b273484097b4bc8bdf66bd1be90f',
+  },
+  {
+    artist: 'Peter Criss',
+    name: 'Peter Criss',
+    releaseYear: 1978,
+    cover: 'https://i.scdn.co/image/ab67616d00001e02365862994e45cba29f27e328',
+  },
+  {
+    artist: 'Paul Stanley',
+    name: 'Paul Stanley',
+    releaseYear: 1978,
+    cover: 'https://i.scdn.co/image/ab67616d0000b2739ac8f304730cf92c54825e69',
+  },
+  {
+    artist: 'Ace Frehley',
+    name: 'Ace Frehley',
+    releaseYear: 1978,
+    cover: 'https://i.scdn.co/image/ab67616d0000b2739383cb896b3ec0967ecaea9f',
+  },
+];
