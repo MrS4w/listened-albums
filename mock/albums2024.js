1f273f966495286b039da2f6819c671d3c56a88e
Victor <adsvictor@outlook.com>
2024-02-05 02:01:13 -0300

    ✨ feat: add 2024 data

---
+++ mock/albums2024.js
@@ -0,0 +1,8 @@
+const albums = [
+  {
+    artist: 'Dirt',
+    name: 'Alice In Chains',
+    releaseYear: 1992,
+    cover: 'https://i.scdn.co/image/ab67616d0000b2731cd131e5e9ad887ad8c1ecb7',
+  },
+];
