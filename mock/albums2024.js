1597be418a9824b4c4b895372c54e60d0f862a72
Victor <adsvictor@outlook.com>
2024-02-14 16:33:59 -0300

    ✨ feat: add De Mysteriis Dom Sathanas

--- mock/albums2024.js
+++ mock/albums2024.js
@@ -5,4 +5,10 @@ const albums = [
     releaseYear: 1992,
     cover: 'https://i.scdn.co/image/ab67616d0000b2731cd131e5e9ad887ad8c1ecb7',
   },
+  {
+    artist: 'Mayhem',
+    name: 'De Mysteriis Dom Sathanas',
+    releaseYear: 1994,
+    cover: 'https://i.scdn.co/image/ab67616d0000b2732b58dcaa52f824711a95ab1b',
+  },
 ];
