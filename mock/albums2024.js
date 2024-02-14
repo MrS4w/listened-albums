90d8e16da8dd7970116dee1792959cf7d67c3f7e
Victor <adsvictor@outlook.com>
2024-02-14 17:58:35 -0300

    ✨ feat: add We Can't Dance

--- mock/albums2024.js
+++ mock/albums2024.js
@@ -1,7 +1,7 @@
 const albums = [
   {
-    artist: 'Dirt',
-    name: 'Alice In Chains',
+    artist: 'Alice In Chains',
+    name: 'Dirt',
     releaseYear: 1992,
     cover: 'https://i.scdn.co/image/ab67616d0000b2731cd131e5e9ad887ad8c1ecb7',
   },
@@ -10,5 +10,11 @@ const albums = [
     name: 'De Mysteriis Dom Sathanas',
     releaseYear: 1994,
     cover: 'https://i.scdn.co/image/ab67616d0000b2732b58dcaa52f824711a95ab1b',
+  },
+  {
+    artist: 'Genesis',
+    name: "We Can't Dance",
+    releaseYear: 1991,
+    cover: 'https://i.scdn.co/image/ab67616d0000b273da85dd2a59ffff7a6817b720',
   },
 ];
