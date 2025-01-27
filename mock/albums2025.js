6535fd3d626129c684f077d4937d66c88c5ab06d
Victor Λntonio <slipknoteirovictor@hotmail.com>
2025-01-27 17:45:32 -0300

    :sparkles: feat: add 2025 data

---
+++ mock/albums2025.js
@@ -0,0 +1,8 @@
+const albums = [
+    {
+      artist: 'Mercyful Fate',
+      name: 'Melissa',
+      releaseYear: 1983,
+      cover: 'https://i.scdn.co/image/ab67616d0000b2732047927ddb02a89e8ec1dd30',
+    },
+]
