9aafc33a8aa2b7e6b6009102cf3de46259047a64
Victor Λntonio <slipknoteirovictor@hotmail.com>
2025-02-02 14:12:41 -0300

    :sparkles: feat: add Don't Break the Oath

--- mock/albums2025.js
+++ mock/albums2025.js
@@ -11,4 +11,10 @@ const albums = [
       releaseYear: 1999,
       cover: 'https://i.scdn.co/image/ab67616d0000b2732b4da5abb231f0dfc297ff09',
     },
+    {
+      artist: 'Mercyful Fate',
+      name: "Don't Break the Oath",
+      releaseYear: 1984,
+      cover: 'https://i.scdn.co/image/ab67616d0000b2734fd0c84a79418987c8a16d50',
+    },
 ]
